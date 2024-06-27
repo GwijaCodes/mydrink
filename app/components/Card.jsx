80cdcb9d711d0f63a20712669d48291ba0d8ca5e
Gloria Saija <blackiedoescomics@gmail.com>
2024-06-27 19:21:17 +0200

    add display of saved from db.json

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import Link from "next/link";
-import Image from 'next/image';
 import Star from './Star'
 import { useState } from 'react';
 
-function Card({ recipe, saved }) {
+function Card({ recipe, url }) {
     const [isFav, setIsFav] = useState(false)
 
     const starred = (e) => {
@@ -19,12 +18,12 @@ function Card({ recipe, saved }) {
     return (
         <Link href={`/recipes/${recipe?.idDrink}`}>
             <div
-                style={{ '--image-url': `url(${recipe?.strDrinkThumb})` }}
+                style={{ '--image-url': `url(${url})` }}
                 className='relative w-64 h-64 max-w-xs overflow-hidden bg-[image:var(--image-url)] bg-cover flex flex-col justify-end'>
                 <div className='w-64 h-64 absolute bg-gradient-to-t from-[--teal] to-transparent-500 to-40%'></div>
-                <h2 className='z-10 font-semibold text-black m-4 text-lg'>{recipe?.strDrink}</h2>
+                <h2 className='z-10 font-semibold text-black m-4 text-lg'>{recipe}</h2>
                 <div className='w-10 h-10 m-4 absolute top-0 right-0'>
-                    <Star className="star" saved={saved} key={recipe?.idDrink} name={recipe?.strDrink} pic={recipe?.strDrinkThumb} />
+                    {/* <Star className="star" key={recipe?.idDrink} name={recipe?.strDrink} pic={recipe?.strDrinkThumb} /> */}
                 </div>
             </div>
         </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -4,40 +4,27 @@ import Header from './components/Header'
 import Search from './components/Search'
 import Card from './components/Card'
 
-export default function Home() {
+export default function Home({ }) {
   const [recipes, setRecipes] = useState([]);
+  const [favs, setFavs] = useState([]);
 
   const [loading, setLoading] = useState(false);
-  const [term, setTerm] = useState('')
-
-  const saved = [
-    {
-      key: 1,
-      recipe: 'Gin Fizz',
-      pic: ''
-    },
-    {
-      key: 2,
-      recipe: 'Gin Lemon',
-      pic: ''
-    },
-  ];
 
   useEffect(() => {
     setLoading(true)
     const fetchRecipes = async () => {
-        try{
+      try {
         const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=gin`)
 
-        if(!res.ok){
+        if (!res.ok) {
           throw new Error('Something went wrong')
         }
         const result = await res.json();
         setRecipes(result?.drinks)
 
         // console.log(recipes)
-        
-      } catch(err){
+
+      } catch (err) {
         console.log(err)
       }
       setLoading(false)
@@ -45,42 +32,52 @@ export default function Home() {
 
     fetchRecipes()
   }, [])
+    
+    useEffect(() => {
+      fetch(`http://localhost:3001/favdrinks`)
+      .then(response => response.json())
+      .then(data => setFavs(data))
+      .catch(err => console.log(err))
+      
+    }, []);
+
+
   return (
     <div className="bg-[--pale]">
-    <Header/>
-    <Search setRecipes={setRecipes}/>
+      <Header />
+      <Search setRecipes={setRecipes} />
 
-    <div className="flex items-center justify-baseline p-10">
-      {
-        loading? (<><h2 className="text-center 3-xl">Loading...</h2></>) : (<>
-        <div className="flex overflow-x-scroll pb-10 hide-scroll-bar gap-5">
+      <div className="flex items-center justify-baseline p-10">
         {
-          recipes?.map((recipe) => (
-            <Card 
-            key={recipe?.idDrink} 
-            recipe={recipe}
-            saved = {saved}/>
-          ))
+          loading ? (<><h2 className="text-center 3-xl">Loading...</h2></>) : (<>
+            <div className="flex overflow-x-scroll pb-10 hide-scroll-bar gap-5">
+              {
+                recipes?.map((recipe) => (
+                  <Card
+                    key={recipe?.idDrink}
+                    recipe={recipe?.strDrink} 
+                    url={recipe?.strDrinkThumb}/>
+                ))
+              }
+            </div>
+          </>)
         }
-      </div>
-        </>)
-      }
-      
-    </div>
 
-    <div className="saved flex items-center justify-baseline p-10">
-      <div className="flex overflow-x-scroll pb-10 hide-scroll-bar gap-5">
- {
-      // saved.map(() => (
-     
-       
-      // )
-
-      // )
-      }
       </div>
-     
-    </div>
+
+      <div className="flex items-center justify-baseline p-10">
+        <div className="flex overflow-x-scroll pb-10 hide-scroll-bar gap-5">
+          {
+            favs?.map((fav) => (
+              <Card
+                key={fav?.id}
+                recipe={fav?.name}
+                url={fav?.url}
+              />
+            ))
+          }
+        </div>
+      </div>
     </div>
 
   );
